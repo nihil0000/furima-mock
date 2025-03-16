8ce8f74a654117372234e1a3747ca70399a2e0e9
nihil0000 <nihil.dev0@gmail.com>
2025-03-16 16:28:14 +0900

    feat: Add routing for authentication, registration, profile, product list, purchase

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
+use Illuminate\Session\Middleware\AuthenticateSession;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthenticatedSessionController;
+use App\Http\Controllers\RegisteredUserController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\PurchaseController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +19,39 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// auth
+Route::controller(AuthenticatedSessionController::class)->group(function () {
+    Route::get('/login', 'create')->name('login.create'); // display login form
+    Route::post('/login', 'store')->name('login.store'); // authentication (login)
+    Route::post('/logout', 'destroy')->name('logout.destroy'); // logout
 });
+
+// register
+Route::controller(RegisteredUserController::class)->group(function () {
+    Route::get('/register', 'create')->name('register.create'); // display register form
+    Route::post('/register', 'store')->name('register.store');  // register
+});
+
+// profile
+Route::controller(ProfileController::class)->group(function () {
+    Route::get('/mypage', 'show')->name('profile.show'); // display the profile
+    Route::get('/mypage/profile', 'edit')->name('profile.edit');  // display to edit　the profile
+    Route::post('/mypage', 'store')->name('profile.store');  // register　the profile
+});
+
+// product
+Route::controller(ProductController::class)->group(function () {
+    Route::get('/', 'index')->name('product.index'); // display the product list
+    Route::get('/product/{product_id}', 'show')->name('product.show'); // display details of product
+    Route::get('/exhibit', 'create')->name('product.create'); // exhibit product
+    Route::post('/exhibit', 'store')->name('product.store'); // register product
+});
+
+// purchase
+Route::controller(PurchaseController::class)->group(function () {
+    Route::get('/purchase/address/{product_id}', 'create')->name('purchase.create');
+    Route::get('/purchase/{product_id}', 'show')->name('purchase.show');
+    Route::post('/purchase', 'update')->name('purchase.update');
+});
+
+
