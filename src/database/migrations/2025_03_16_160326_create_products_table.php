ce94bcafdcbf1e630b719c88c796239dfe7e42ee
nihil0000 <nihil.dev0@gmail.com>
2025-03-16 16:17:31 +0900

    feat: Create tables for users, addressed, categories, payments, products, product_category, favorites, comments, orders

---
+++ src/database/migrations/2025_03_16_160326_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->string('product_name', 255);
+            $table->string('brand_name', 255);
+            $table->unsignedInteger('price');
+            $table->text('description');
+            $table->string('status', 50);
+            $table->string('image', 255)->nullable();
+            $table->boolean('is_sold')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
