a6c40ae2ca920f433500dc1be73f9802ee1c8fef
nihil0000 <nihil.dev0@gmail.com>
2025-03-23 10:37:53 +0900

    feat: Enhance product list display function and add logic to get users' favorite products

--- src/app/Http/Controllers/ProductController.php
+++ src/app/Http/Controllers/ProductController.php
@@ -4,16 +4,59 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Product;
+use Illuminate\Support\Facades\Auth;
 
 class ProductController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        return view('product.index');
+        $tab = $request->query('tab', 'recommend');
+        $query = $request->query('query');
+
+        if ($tab === 'mylist') {
+            if (!Auth::check()) {
+                return redirect()->guest(route('login.create'));
+            }
+
+            /** @var \App\Models\User $user */
+            $user = Auth::user();
+
+            // Apply search scope to product
+            $favorites = $user->favorites()
+                ->whereHas('product', function ($q) use ($query) {
+                    if (!empty($query)) {
+                        $q->search($query);
+                    }
+
+                    if (Auth::check()) {
+                        $q->excludeOwn(Auth::id());
+                    }
+                })
+                ->with('product')
+                ->get();
+
+            return view('product.index', compact('favorites', 'query'));
+        }
+
+        $products =Product::query();
+
+        if (Auth::check()) {
+            $products->excludeOwn(Auth::id());
+        }
+
+        if (!empty($query)) {
+            $products->search($query);
+        }
+
+        $products = $products->get();
+
+        return view('product.index', compact('products', 'query'));
     }
 
     public function show(Product $product)
     {
+        $product->with('categories')->get();
+
         return view('product.show', compact('product'));
     }
 
