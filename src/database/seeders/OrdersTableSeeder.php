fade7bded3372f58c6e50fd25b4a176db22f915f
nihil0000 <nihil.dev0@gmail.com>
2025-03-24 10:43:51 +0900

    feat: Change the number of orders generated by OrdersTableSeeder from 10 to 3

--- src/database/seeders/OrdersTableSeeder.php
+++ src/database/seeders/OrdersTableSeeder.php
@@ -14,6 +14,6 @@ class OrdersTableSeeder extends Seeder
      */
     public function run()
     {
-        Order::factory()->count(10)->create();
+        Order::factory()->count(3)->create();
     }
 }
